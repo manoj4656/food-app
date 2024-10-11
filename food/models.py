aa427b5c1e3fc4ed4bf47562939efc9309537723
manoj <manoj@example.com>
2024-10-12 02:07:54 +0530

    added text filed and image field to model and change detail page ui

--- food/models.py
+++ food/models.py
@@ -5,6 +5,8 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
+    item_prep = models.TextField(default='there is no data on this item')
 
     def __str__(self):
         return self.item_name
--- food/urls.py
+++ food/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'food'
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('<int:item_id>',views.detail,name='detail'),
+    path('',views.index, name='index'),
+    path('<int:item_id>',views.detail, name='detail'),
 ]
